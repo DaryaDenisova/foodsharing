41e3448af219f7c9b6a6f01a985f37c984208daa
DaryaDenisova <dsdenisova@edu.hse.ru>
2018-11-20 13:24:53 +0300

    database

--- db.py
+++ db.py
@@ -5,3 +5,33 @@ users = {
         'place': 'HSE'
     }
 }
+
+_users_list = list(_users.values())
+
+
+_user_list = []
+
+for login, user_data in _users.items():
+    _new_element = {'login': login}
+    _new_element.update(user_data)
+    _user_list.append(_new_element)
+
+
+
+
+
+#get users filtered by name
+def get_users_by_name(q):
+    results =[]
+    # SEARCH
+
+    for user in _user_list:
+        if user['name'].lower().find(q.lower()) >=0:
+            results.append(user)
+
+    return results
+
+
+
+def get_users(username)
+    return _users.get(username)
