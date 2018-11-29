4923c30c8b07f181df8c727b4b70d2ea217f7228
qqlizard <lizardard108@gmail.com>
2018-11-29 17:43:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,12 @@ def offer():
 def register():
     return render_template('register page.html')
 
+@app.route('/search')
+def search_for_person():
+    q = request.args.get('query')
+    users = db.get_users_by_name(q)
+    return render_template('search_results.html', q=q, users=users)
+
 app.run(port=5000)
 
 
