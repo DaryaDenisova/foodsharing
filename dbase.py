86dcf80e48b516c151c876f959831828f1db33f4
qqlizard <lizardard108@gmail.com>
2018-12-11 12:57:58 +0300

    test

--- dbase.py
+++ dbase.py
@@ -15,13 +15,15 @@ CREATE TABLE offer(
     category TEXT,
     description TEXT,
     location TEXT,
-    giver_id INTEGER,
-    date of the deal DATE,
-    time of the deal TIME,
-    FOREIGN KEY (taker_id) REFERENCES users(user_id), 
-    FOREIGN KEY (giver_id) REFERENCES users(user_id)
+    station_id INTEGER, 
+    giver_id INTEGER,  
+    date TEXT,
+    time TEXT,
+    FOREIGN KEY (giver_id) REFERENCES users(user_id),
+    FOREIGN KEY (station_id) REFERENCES metro_station (station_id)
 )
 ''')
+
 
 c.execute('''
 CREATE TABLE users(
@@ -51,21 +53,40 @@ CREATE TABLE deal_agreement(
 )
 ''')
 
+
+c.execute('''
+CREATE TABLE metro_station(
+    station_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    station_name TEXT
+)
+''')
+
 conn.commit()
 # adding some data
 
 c.execute ('''
-    INSERT INTO offer (offer_id, name, category, description, location, giver_id, taker_id, time of the deal)
-    VALUES ('1', 'chocolate', 'sweet', 'ostalos mnogo shokolada s novogo goda', 'nevsky prospekt', '10', '4', '15/01, 19:00'),
-    ('2', 'tomatoes amd cucumbers', 'vegetable', 'svoi ovoschi s gryadki', 'kupchino', '5', '8', '08/08, 12:00'),
-    ('3', 'apples', 'fruit', 'narvali u sosedeq, ochen vkusnye', 'prospekt veteranov', '12', '20', '17/09, 10:00'),
-    ('4', 'bulochki', 'bread', 'darim schastiye i svejie bulochki v vash dom', 'pekarnya volcheka', '2', '13', '12/12, 23:00'),
-    ('5', 'porridge', 'croup', 'navarila kashi na ves rayon', 'kanal griboedova', '1', '7', '21/12, 11:00'),
-    ('6', 'zubatka', 'fish', 'zapaha vrode net', 'metro zvezdnaya', '11', '14', '11/12, 20:00'),
-    ('7', 'sea kale', 'sea food', 'pokazalas ne vkusnoy', 'rybatskoe', '2', '9', '10/01, 21:00'),
-    ('8', 'wine', 'alcohol', 'otdam 2 butylki krasnogo', 'devyatkino', '6', '3', '02/02, 18:00'),
-    ('9', 'provencal herbs', 'spice', 'klevye travy, no ne na moy vkus', 'petrogradskiy district', '22', '21', '16/10, 14:00'),
-    ('10', 'pumpkin seeds', 'nuts and seeds', 'ya takoye ne em', 'ulitsa belgradskaya', '16', '19', '14/11, 19:00');
+    INSERT INTO offer (name, category, description, location, giver_id,  date, time)
+    VALUES ('chocolate', 'sweet', 'ostalos mnogo shokolada s novogo goda', 'nevsky prospekt', '10', '15.01.2018', '19:00'),
+    ('tomatoes amd cucumbers', 'vegetable', 'svoi ovoschi s gryadki', 'kupchino', '5', '08.08.2018', '12:00'),
+    ('apples', 'fruit', 'narvali u sosedeq, ochen vkusnye', 'prospekt veteranov', '12', '17.09.2018', '10:00'),
+    ('bulochki', 'bread', 'darim schastiye i svejie bulochki v vash dom', 'pekarnya volcheka', '2', '12.12.2018', '23:00'),
+    ('porridge', 'croup', 'navarila kashi na ves rayon', 'sennaya ploshad', '1', '21.12.2018', '11:00'),
+    ('zubatka', 'fish', 'zapaha vrode net', 'zvezdnaya', '11', '11.12.2018', '20:00'),
+    ('sea kale', 'sea food', 'pokazalas ne vkusnoy', 'rybatskoe', '2', '10.01.2018', '21:00'),
+    ('wine', 'alcohol', 'otdam 2 butylki krasnogo', 'devyatkino', '6', '02.02.2018', '18:00'),
+    ('provencal herbs', 'spice', 'klevye travy, no ne na moy vkus', 'petrogradskaya', '22', '16.10.2018', '14:00'),
+    ('pumpkin seeds', 'nuts and seeds', 'ya takoye ne em', 'mejdunarodnaya', '16', '14.11.2018', '19:00'),
+    ('semena', 'nuts and seeds', 'godnota dlya popugov', 'lomonosovskaya', '17', '18.11.2018', '14:00'), 
+    ('potato soup', 'ready mix', 'ot babushki', 'avtovo', '11','02.09.2018', '14:30'), 
+    ('pertushka', 'green', 'iz svoego ogoroda', 'zvenigorodskaya', '10', '11.11.2018', '15:30'), 
+    ('izabella', 'wine', 'podarili na prazdnik', 'dostoevskaya', '7', '7.08.2018', '18:30'), 
+    ('granatovoye', 'wine', 'ne lublu vino', 'parnas', '6', '14.09.2018', '18:20'), 
+    ('water melon', 'fruits and vegetables', 'ochen mnogo na dache', 'devyatkino', '3', '19.08.2018', '10:20'), 
+    ('cucumbers', 'fruits and vegetables', 's dachi vezy', 'devyatkino', '9', '19.08.2018', '11:40'), 
+    ('choco bins', 'nuts and seeds', 'podarili a u menya allergiya', 'devyatkino', '2', '20.10.2018', '12:00'), 
+    ('moloko', 'dairy', 'iz nastoyashi korovy', 'sennaya ploschad', '12', '20.10.2018', '11:00'), 
+    ('bulka hleba', 'bread', 'pud hleba razdaet podarki', 'nevsky prospekt', '8', '20.10.2018', '13:30'), 
+    ('choco bins', 'nuts and seeds', 'priverly iz kitaya', 'sadovaya', '11', '20.10.2018', '14:00');
 
 ''')
 #MANY TO MANY
@@ -80,3 +101,74 @@ c.execute('''
 ''')
 
 conn.commit()
+
+c.execute('''
+INSERT INTO metro_station (station_id, station_name) 
+VALUES ('1', 'Devyatkino'), 
+('2', 'Grazhdansky Prospekt'), 
+('3', 'Akademicheskaya'), 
+('4', 'Politekhnicheskaya'), 
+('5', 'Ploschad Muzhestva'), 
+('6', 'Lesnaya'), 
+('7', 'Vyborgskaya'), 
+('8', 'Ploshchad Lenina'), 
+('9', 'Chernyshevskaya'), 
+('10', 'Ploshchad Vosstaniya'), 
+('11', 'Vladimirskaya'), 
+('12', 'Pushkinskaya'), 
+('13', 'Baltiyskaya'), 
+('14', 'Narvskaya'), 
+('15', 'Kirovsky Zavod'), 
+('16', 'Avtovo'), 
+('17', 'Leninsky Prospekt'), 
+('18', 'Prospekt Veteranov'), 
+('19', 'Parnas'), 
+('20', 'Prospect Prosvesheniya'), 
+('21', 'Ozerki'), 
+('22', 'Udelnaya'), 
+('23', 'Pionerskaya'), 
+('24', 'Chyornaya Rechka'), 
+('25', 'Petrogradskaya'), 
+('26', 'Gorkovskaya'), 
+('27', 'Nevsky Prospekt'), 
+('28', 'Sennaya Ploschad'), 
+('29', 'Tekhnologichesky Institut '), 
+('30', 'Frunzenskaya'), 
+('31', 'Moskovskiye Vorota'), 
+('32', 'Elektrosila'), 
+('33', 'Park Pobedy'), 
+('34', 'Moskovskaya'), 
+('35', 'Zvyozdnaya'), 
+('36', 'Kupchino'), 
+('37', 'Begovaya'),
+('38', 'Novokrestovskaya'),
+('39', 'Primorskaya'), 
+('40', 'Vasileostrovskaya'), 
+('41', 'Gostiny Dvor'), 
+('42', 'Mayakovskaya'), 
+('43', 'Ploshchad Alexandra Nevskogo'), 
+('44', 'Yelizarovskaya'), 
+('45', 'Lomonosovskaya'), 
+('46', 'Proletraskaya'), 
+('47', 'Obukhovo'), 
+('48', 'Rybatskoye'), 
+('49', 'Spasskaya'), 
+('50', 'Dostoevskaya'), 
+('51', 'Ligovsky Prospekt'), 
+('52', 'Novocherkasskaya'), 
+('53', 'Ladozhskaya'), 
+('54', 'Prospekt Bolshevikov'), 
+('55', 'Ulitsa Dybenko'), 
+('56', 'Komendantskiy Prospekt'), 
+('57', 'Staraya Derevnya'), 
+('58', 'Krestovsky Island'), 
+('59', 'Chkalovskaya'), 
+('60', 'Sportivnaya'), 
+('61', 'Admiralteyskaya'), 
+('62', 'Sadovaya'), 
+('63', 'Zvenigorodskaya'), 
+('64', 'Obvodny Kanal'), 
+('65', 'Volkovskaya'), 
+('66', 'Bukharestskaya'), 
+('67', 'Mezhdunarodnaya');
+''')
